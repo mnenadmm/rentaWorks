7de0ad4940754df42bf72e97b6bb5f547d481fd4
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-28 22:55:18 +0200

    sredjivanje pocetnedd

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { PocetnaComponent } from './components/pocetna/pocetna.component';
 export const routes: Routes = [
+    { path: '', component: PocetnaComponent },
     { path: 'login', component: LoginComponent },
-    { path: '/', component: PocetnaComponent },
+    
 ];
