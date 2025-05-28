d5ce08200e04213abfd496a0fe5d9161359a440f
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-28 22:53:56 +0200

    sredjivanje pocetned

--- client/src/app/components/pocetna/pocetna.component.ts
+++ client/src/app/components/pocetna/pocetna.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pocetna',
+  standalone: true,
   imports: [],
   templateUrl: './pocetna.component.html',
   styleUrl: './pocetna.component.css'
