e06b6f5b61af0dc3a412bb999eaa69b77c011ae5
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-28 22:43:50 +0200

    login pocetak

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+];
