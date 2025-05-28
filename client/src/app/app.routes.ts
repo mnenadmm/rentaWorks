d1aea3b46152e6de5e0be746aff730152c13dce2
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-28 23:09:49 +0200

    register ruta

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { PocetnaComponent } from './components/pocetna/pocetna.component';
+import { RegistracijaComponent } from './auth/registracija/registracija.component';
 export const routes: Routes = [
     { path: '', component: PocetnaComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegistracijaComponent },
     
 ];
---
+++ client/src/app/auth/registracija/registracija.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registracija',
+  standalone: true,
+  imports: [],
+  templateUrl: './registracija.component.html',
+  styleUrl: './registracija.component.css'
+})
+export class RegistracijaComponent {
+
+}
