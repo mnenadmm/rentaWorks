e53563b885255463d6647d0caa9e042373d6735a
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-28 23:02:57 +0200

    pocetna rute

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],  // ispravljeno ovde
-  imports: [HeaderComponent, FooterComponent]
+  imports: [RouterModule,HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'client';
