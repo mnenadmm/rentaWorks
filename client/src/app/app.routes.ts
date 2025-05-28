ec9fea360b6d3f9e7195d8b38e075183a221bdb3
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-28 23:16:47 +0200

    about

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { PocetnaComponent } from './components/pocetna/pocetna.component';
 import { RegistracijaComponent } from './auth/registracija/registracija.component';
+import { AboutComponent } from './pages/about/about.component';
 export const routes: Routes = [
     { path: '', component: PocetnaComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegistracijaComponent },
+    { path: 'about', component: RegistracijaComponent },
     
 ];
