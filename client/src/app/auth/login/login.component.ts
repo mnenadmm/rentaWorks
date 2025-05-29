2b3ab8fc28cc38002cbf9a299e9ddf1397963590
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-29 22:58:00 +0200

    login tri pokusaja

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -13,6 +13,11 @@ export class LoginComponent {
   password: string = '';
   showPassword: boolean = false;
   errorMessage: string = '';
+  maxAttempts: number = 3;
+  loginAttempts: number = 0;
+  isLockedOut: boolean = false;
+  lockoutTime: number = 30; // sekundi
+  remainingTime: number = this.lockoutTime;
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
@@ -21,12 +26,43 @@ export class LoginComponent {
   onLogin() {
     // Resetuj error poruku pre provere
     this.errorMessage = '';
-
-    if (this.username && this.password) {
-      console.log('Prijava uspešna:', this.username, this.password);
-     this.errorMessage = 'Korisničko ime ili lozinka nisu ispravni. Molimo pokušajte ponovo.';
-    } else {
-     this.errorMessage = 'Korisničko ime ili lozinka nisu ispravni. Molimo pokušajte ponovo.';
+    if (this.isLockedOut) {
+      this.errorMessage = `Previše neuspešnih pokušaja. Pokušajte ponovo za ${this.remainingTime} sekundi.`;
+      return;
     }
+    if (this.username && this.password) {
+      const isValid = this.username === 'admin' && this.password === 'admin123';
+    if (isValid){
+       console.log('Prijava uspešna:', this.username);
+        this.loginAttempts = 0;
+        this.errorMessage = '';
+    }else{
+        this.loginAttempts++;
+        if (this.loginAttempts >= this.maxAttempts){
+            this.lockUserOut();
+        }else{
+           this.errorMessage = 'Korisničko ime ili lozinka nisu ispravni. Molimo pokušajte ponovo.';
+        }
+    }
+    } else {
+     this.errorMessage = 'Korisničko ime ili lozinka nisu ispravni. ';
+    }
+  }
+  lockUserOut() {
+    this.isLockedOut = true;
+    this.errorMessage = `Previše neuspešnih pokušaja. Pokušajte ponovo za ${this.lockoutTime} sekundi.`;
+    this.remainingTime = this.lockoutTime;
+
+    const interval = setInterval(() => {
+      this.remainingTime--;
+      this.errorMessage = `Previše neuspešnih pokušaja. Pokušajte ponovo za ${this.remainingTime} sekundi.`;
+
+      if (this.remainingTime <= 0) {
+        clearInterval(interval);
+        this.isLockedOut = false;
+        this.loginAttempts = 0;
+        this.errorMessage = '';
+      }
+    }, 1000);
   }
 }
