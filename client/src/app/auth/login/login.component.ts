076ab832195ec6fde5f7a4a769e245c3c6f516a5
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-29 22:38:38 +0200

    login sredjivanje forme

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
@@ -12,17 +12,21 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   showPassword: boolean = false;
+  errorMessage: string | null = 'Uneli ste pogresno korisnicko ime ili lozinku';
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
   onLogin() {
+    // Resetuj error poruku pre provere
+    this.errorMessage = null;
+
     if (this.username && this.password) {
       console.log('Prijava uspešna:', this.username, this.password);
       // Ovde možeš emitovati event, pozvati servis, itd.
     } else {
-      alert('Molimo unesite korisničko ime i lozinku.');
+      this.errorMessage = 'Molimo unesite korisničko ime i lozinku.';
     }
   }
 }
