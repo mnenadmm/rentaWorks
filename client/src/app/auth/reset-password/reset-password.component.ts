ba5774ba458968d545f0a0f9df403a5ebf9859da
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-30 18:30:45 +0200

    add routerMOdulea

--- client/src/app/auth/reset-password/reset-password.component.ts
+++ client/src/app/auth/reset-password/reset-password.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,  RouterModule],
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
