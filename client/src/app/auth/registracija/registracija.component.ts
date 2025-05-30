c120c294c16811517329c33243dde2ad8f1823b9
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-30 22:11:30 +0200

    forma za registraciju 1

--- client/src/app/auth/registracija/registracija.component.ts
+++ client/src/app/auth/registracija/registracija.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registracija',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],  // DODATO ReactiveFormsModule
   templateUrl: './registracija.component.html',
-  styleUrl: './registracija.component.css'
+  styleUrls: ['./registracija.component.css']
 })
-export class RegistracijaComponent {
+export class RegistracijaComponent implements OnInit {
+  korak = 1;
+  ukupnoKoraka = 4;
+
+  formaKorak1!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formaKorak1 = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      ime: ['', Validators.required],
+      prezime: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      lozinka: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  sledeciKorak() {
+    if (this.korak === 1) {
+      if (this.formaKorak1.invalid) {
+        this.formaKorak1.markAllAsTouched();
+        return;
+      }
+    }
+
+    if (this.korak < this.ukupnoKoraka) {
+      this.korak++;
+    }
+  }
+
+  prethodniKorak() {
+    if (this.korak > 1) {
+      this.korak--;
+    }
+  }
+  submitRegistraciju() {
+  // Ovde ide logika za završetak registracije, slanje podataka na backend itd.
+  // Za sada samo prikazujemo alert i resetujemo korake:
+  alert('Registracija je završena!');
+  this.korak = 1;
+  this.formaKorak1.reset();
+}
 
 }
