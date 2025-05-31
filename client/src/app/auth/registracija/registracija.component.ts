9ef3b08a77f0c3ff2979fdca2015068445c69ace
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-31 21:07:51 +0200

    19

--- client/src/app/auth/registracija/registracija.component.ts
+++ client/src/app/auth/registracija/registracija.component.ts
@@ -26,7 +26,7 @@ export class RegistracijaComponent  {
       ime: ['', Validators.required],
       prezime: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', Validators.required],
     }, { validators: this.passwordMatchValidator });
   }
