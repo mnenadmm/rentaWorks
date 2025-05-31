28a3c1487239998c803a6c8b9000636f3549e78b
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-31 19:19:42 +0200

    9

--- client/src/app/auth/registracija/registracija.component.ts
+++ client/src/app/auth/registracija/registracija.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registracija',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,RouterModule],
   templateUrl: './registracija.component.html',
   styleUrls: ['./registracija.component.css']
 })
