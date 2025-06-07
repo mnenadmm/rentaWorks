edb81d25392537abca133347bfe57445009f3fea
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-07 17:54:09 +0200

    a1

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-footer',
+  standalone:false,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
