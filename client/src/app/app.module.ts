b10fdab40832fad5fe452d87efe7c718f4254791
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-07 19:30:37 +0200

    a5233ed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,18 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegistracijaModule } from './auth/registracija/registracija.module';  // ako je modul
-import { routes } from './app.routes';  // tvoje rute
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 import { RegistracijaComponent } from './auth/registracija/registracija.component';
 import { LoginComponent } from './auth/login/login.component';
-
+//Uvozimo appRoutingMOdule koji sam sam kreirao
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,9 +26,9 @@ import { LoginComponent } from './auth/login/login.component';
     // ostale komponente ako ih imaš direktno
   ],
   imports: [
+     AppRoutingModule, // uključi routing ovde
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes),
     RegistracijaModule,
     ReactiveFormsModule,
     // ostali moduli
