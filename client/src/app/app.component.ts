8a61e64d49b52a5b64d6325a48d2db9cee15c7fd
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-08 09:01:02 +0200

    a5233edd

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, HostListener, AfterViewInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
