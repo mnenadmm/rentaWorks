d66efe7a44da013375909baf7b032b81c8e2c8e8
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-08 18:21:16 +0200

    registracija dodavanje rute

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { RegistracijaComponent } from './authentication/registracija/registracija.component';
 import { ResetPasswordComponent } from './authentication/reset-password/reset-password.component';
 const routes: Routes = [
      { path: 'login', component: LoginComponent },
-     { path: 'reset-password', component: ResetPasswordComponent }
+     { path: 'reset-password', component: ResetPasswordComponent },
+     { path: 'register', component: RegistracijaComponent }
 ];
 
 @NgModule({
