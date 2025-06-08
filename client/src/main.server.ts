915d929286b06042df843bba068b57bb6d8c7c9a
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-08 09:23:14 +0200

    a5233eddd

--- client/src/main.server.ts
+++ client/src/main.server.ts
@@ -1,6 +1,7 @@
 import { platformServer } from '@angular/platform-server';
 import { AppModule } from './app/app.module';
+import { AppServerModule } from './app/app.server.module';
 
-export const bootstrap = () => platformServer().bootstrapModule(AppModule);
+export const bootstrap = () => platformServer().bootstrapModule(AppServerModule);
 
 export default bootstrap;
