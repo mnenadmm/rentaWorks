f7539ef7192bd4bc1f5d26dd813757d62034e10c
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-08 17:28:38 +0200

    header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({  
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+setMenuState(open: boolean) {
+  this.menuOpen = open;
+  document.body.classList.toggle('menu-open', open);
+}
+
+toggleMenu() {
+  this.setMenuState(!this.menuOpen);
+}
+
+closeMenu() {
+  this.setMenuState(false);
+}
 }
