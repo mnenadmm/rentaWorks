b0da70785f1eb4439903697d3445b0e4f178a361
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-10 21:23:56 +0200

    a5233eddwwwssssssdfss

--- frontend/src/app/components/custom-datepicker/custom-datepicker.component.ts
+++ frontend/src/app/components/custom-datepicker/custom-datepicker.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
 
 @Component({
   selector: 'app-custom-datepicker',
@@ -11,16 +11,36 @@ export class CustomDatepickerComponent {
   currentMonth: number; // 0-based (0 = januar)
   days: number[] = [];
   selectedDate?: Date;
-
+   showCalendar = false;
+  showYearPicker = false;
+  years: number[] = [];
   @Output() dateSelected = new EventEmitter<Date>();
 
-  constructor() {
+  constructor(private eRef: ElementRef) {
     const today = new Date();
     this.currentYear = today.getFullYear();
     this.currentMonth = today.getMonth();
     this.generateCalendar();
+    this.generateYears();
+    
   }
-
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.showCalendar = false;
+      this.showYearPicker = false;
+    }
+  }
+  toggleCalendar() {
+    this.showCalendar = !this.showCalendar;
+    this.showYearPicker = false;
+  }
+    generateYears() {
+    const current = new Date().getFullYear();
+    for (let year = current; year >= 1930; year--) {
+      this.years.push(year);
+    }
+  }
   generateCalendar() {
     // Broj dana u mesecu
     const daysInMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
@@ -46,13 +66,34 @@ export class CustomDatepickerComponent {
     }
     this.generateCalendar();
   }
-
+  isFutureDate(day: number): boolean {
+  const date = new Date(this.currentYear, this.currentMonth, day);
+  const today = new Date();
+  // Poništavanje vremena da poređenje bude po datumu
+  today.setHours(0, 0, 0, 0);
+  date.setHours(0, 0, 0, 0);
+  return date > today;
+}
   selectDay(day: number) {
-    this.selectedDate = new Date(this.currentYear, this.currentMonth, day);
-    this.dateSelected.emit(this.selectedDate);
+    const selected = new Date(this.currentYear, this.currentMonth, day);
+    const today = new Date();
+
+    if (selected <= today) {
+      this.selectedDate = selected;
+      this.dateSelected.emit(selected);
+      this.showCalendar = false;
+    }
   }
 
   getMonthName() {
     return new Date(this.currentYear, this.currentMonth).toLocaleString('default', { month: 'long' });
+  }
+   toggleYearPicker() {
+    this.showYearPicker = !this.showYearPicker;
+  }
+  selectYear(year: number) {
+    this.currentYear = year;
+    this.showYearPicker = false;
+    this.generateCalendar();
   }
 }
