e59e76f2fc48be727e103760050462e885898aa3
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-11 19:49:17 +0200

    kalendar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component , HostListener, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: false,
+  styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Front';
+
+  setAppContainerHeight() {
+    // Provera da li se izvršavamo u browseru
+    if (typeof document !== 'undefined' && typeof window !== 'undefined') {
+      const appContainer = document.querySelector('.app-container') as HTMLElement;
+      if (appContainer) {
+        appContainer.style.height = `${window.innerHeight}px`;
+      }
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.setAppContainerHeight();
+  }
+
+  @HostListener('window:orientationchange')
+  onOrientationChange() {
+    this.setAppContainerHeight();
+  }
+
+  ngAfterViewInit() {
+    this.setAppContainerHeight();
+  }
 }
