5ee13f6891fc37a9b9bde3660116ea89d5b1bac0
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-15 00:25:15 +0200

    resavanje problema print

--- client/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { platformServer } from '@angular/platform-server';
-import { AppModule } from './app/app.module';
-import { AppServerModule } from './app/app.server.module';
-
-export const bootstrap = () => platformServer().bootstrapModule(AppServerModule);
-
-export default bootstrap;
