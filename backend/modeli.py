539fed4196cb85ae939fb214217ce2c406597c47
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-14 21:22:26 +0200

    zanimanje

--- backend/modeli.py
+++ backend/modeli.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import enum
 
 class TipKorisnika(enum.Enum):
-    radnik = "radnik"
-    firma = "firma"
+    fizicko_lice = "fizicko_lice"
+    pravno_lice = "pravno_lice"
     admin = "admin"
 korisnik_zanimanje = db.Table(
     'korisnik_zanimanje',
@@ -30,6 +30,7 @@ class Korisnik(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     lozinka = db.Column(db.String(128), nullable=False)
     tip_korisnika = db.Column(db.Enum(TipKorisnika), nullable=False)
+    datum_rodjenja = db.Column(db.Date, nullable=True)
     datum_registracije = db.Column(db.DateTime, default=datetime.utcnow)
     aktivan = db.Column(db.Boolean, default=True)
     verifikovan_email = db.Column(db.Boolean, default=False)
@@ -39,13 +40,15 @@ class Korisnik(db.Model):
     biografija = db.Column(db.Text, nullable=True)
     grad = db.Column(db.String(100), nullable=True)
     adresa = db.Column(db.String(255), nullable=True)
-    drzava = db.Column(db.String(100), nullable=True)
+    broj_adrese = db.Column(db.String(10), nullable=True)
+    drzavljanstvo = db.Column(db.String(100), nullable=True)
     telefon = db.Column(db.String(30), nullable=True)
     block_user = db.Column(db.Boolean, default=False, nullable=False)
     prva_rola = db.Column(db.Boolean, default=False, nullable=False)
     druga_rola = db.Column(db.Boolean, default=False, nullable=False)
     treca_rola = db.Column(db.Boolean, default=False, nullable=False)
     zanimanja = db.relationship('Zanimanje', secondary='korisnik_zanimanje', backref='korisnici')
+    vestine = db.relationship('Vestina', secondary='korisnik_vestina', backref='korisnici')
     firme = db.relationship('Firma', backref='vlasnik')  # lista firmi za korisnika
     def to_dict(self):
         return {
@@ -71,6 +74,15 @@ class Korisnik(db.Model):
         }
     def __repr__(self):
         return f"<Korisnik {self.ime} {self.prezime}>"
+
+class Vestina(db.Model):
+    __tablename__ = 'vestine'
+    id = db.Column(db.Integer, primary_key=True)
+    naziv = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<Vestina {self.naziv}>"
+
 #Tabela sadrzi podatke o firmi
 class Firma(db.Model):
     __tablename__ = "firme"
