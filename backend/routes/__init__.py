aa15b069452aff2ad443a2efaac35e9eaaec6136
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-16 22:42:36 +0200

    konekcija ka bazi

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -3,5 +3,5 @@ from .auth_routes import auth_bp
 from .reference import reference_bp
 
 def register_blueprints(app):
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(reference_bp)
+    app.register_blueprint(auth_bp, url_prefix='/api')
+    app.register_blueprint(reference_bp, url_prefix='/api')
