d7449f0656e878813bece76a3b3a6b82fd6d4539
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-16 22:25:44 +0200

    konekcija ka bazi

--- backend/applicationSetup.py
+++ backend/applicationSetup.py
@@ -10,13 +10,14 @@ from routes import register_blueprints
 migrate = Migrate()
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "http://5.75.164.111:4200"}}, supports_credentials=True)
+    #CORS(app, resources={r"/*": {"origins": "http://5.75.164.111:4200"}}, supports_credentials=True)
     # Učitaj DATABASE_URL iz .env
     app.config['SQLALCHEMY_DATABASE_URI'] = get_database_uri()
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     migrate.init_app(app, db)
     register_blueprints(app)
+    CORS(app, supports_credentials=True)
     import modeli
     with app.app_context():
         upgrade()  # Primeni migracije (safe)
