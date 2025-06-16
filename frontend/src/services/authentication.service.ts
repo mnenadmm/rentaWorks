72ba00932e51efebd75667d108d6713ce8e1ec42
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-16 19:58:12 +0200

    resavanje problema sa cors

--- frontend/src/services/authentication.service.ts
+++ frontend/src/services/authentication.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private apiUrl = "http://5.75.164.111:5001/"
-  constructor(private http :HttpClient) { }
+  private apiUrl = "http://5.75.164.111:5001/";
+  readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+    withCredentials: true
+  };
+
+  constructor(private http: HttpClient) { }
+
   register(userData: any): Observable<any> {
-    // POST na backend endpoint za registraciju
-    return this.http.post(`${this.apiUrl}registracija`, userData, { withCredentials: true });
+    // POST na backend endpoint za registraciju sa httpOptions
+    return this.http.post(`${this.apiUrl}registracija`, userData, this.httpOptions);
   }
 }
