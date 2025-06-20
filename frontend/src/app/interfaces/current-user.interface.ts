333373d6b4195808b428c1a3c515bb49255a1085
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-20 19:00:41 +0200

    profil komponenta

--- frontend/src/app/interfaces/current-user.interface.ts
+++ frontend/src/app/interfaces/current-user.interface.ts
@@ -8,16 +8,17 @@ export interface CurrentUserInterface {
   username: string;
   tip_korisnika: 'admin' | 'pravno_lice' | 'fizicko_lice';
   aktivan: boolean;
-  ime: string;
-  prezime: string;
-  email: string;
-  telefon: string;
-  adresa: string;
-  grad: string;
-  drzava: string;
-  datum_registracije: string;
-  biografija: string | null;
-  verifikovan_email: boolean;
-  profilna_slika: string | null;
+  ime?: string;
+  prezime?: string;
+  email?: string;
+  telefon?: string;
+  adresa?: string;
+  grad?: string;
+  drzava?: string;
+  datum_registracije?: string;
+  profilna_slika?: string | null;
+  biografija?: string | null;
+  verifikovan_email?: boolean;
 }
+
 
