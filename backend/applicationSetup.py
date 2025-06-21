de5b49e6afaf2178502b61a56979de48d9063790
stefan <stefan.milosevic@rgz.gov.rs>
2025-06-22 00:47:13 +0200

    profil komponenta

--- backend/applicationSetup.py
+++ backend/applicationSetup.py
@@ -10,13 +10,13 @@ from datetime import timedelta
 migrate = Migrate()
 def create_app():
     app = Flask(__name__)
-    CORS(app, supports_credentials=True)
+    #CORS(app, supports_credentials=True)
     jwt = JWTManager(app)
     app.config['JWT_SECRET_KEY'] = 'moj_tajni_kljuc'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)
     #korisnik ce nakon 7 dana morati opet da se uloguje
     app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=7)
-    #CORS(app, resources={r"/*": {"origins": "http://5.75.164.111:4200"}}, supports_credentials=True)
+    CORS(app, resources={r"/*": {"origins": "http://5.75.164.111:4200"}}, supports_credentials=True)
     # Učitaj DATABASE_URL iz .env
     app.config['SQLALCHEMY_DATABASE_URI'] = get_database_uri()
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
